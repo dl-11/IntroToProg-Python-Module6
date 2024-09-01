9df2aa25010a88dd728d112392eb45ea84571a9d
DL <176932570+dl-11@users.noreply.github.com>
2024-09-01 15:32:33 -0700

    Add files via upload
    Converted to using functions with parameters

--- Assignment06.py
+++ Assignment06.py
@@ -7,7 +7,9 @@
 # ------------------------------------------------------------------------------------------ #
 import json
 
-# Define the Data Constants
+# Define the Data Variables and constants
+FILE_NAME: str = "Enrollments.json"
+students: list = []
 MENU: str = '''
 ---- Course Registration Program ----
   Select from the following menu:  
@@ -17,60 +19,67 @@ MENU: str = '''
     4. Exit the program.
 ----------------------------------------- 
 '''
-# Define the Data Constants
-# FILE_NAME: str = "Enrollments.csv"
-FILE_NAME: str = "Enrollments.json"
 
-# Define the Data Variables and constants
-student_first_name: str = ''  # Holds the first name of a student entered by the user.
-student_last_name: str = ''  # Holds the last name of a student entered by the user.
-course_name: str = ''  # Holds the name of a course entered by the user.
-student_data: dict = {}  # one row of student data
-students: list = []  # a table of student data
-csv_data: str = ''  # Holds combined string data separated by a comma.
-json_data: str = ''  # Holds combined string data in a json format.
-file = None  # Holds a reference to an opened file.
-menu_choice: str  # Hold the choice made by the user.
 
-# When the program starts, read the file data into a list of lists (table)
-# Extract the data from the file
-try:
-    file = open(FILE_NAME, "r")
-
-    # CSV Answer
-    # for row in file.readlines():
-    #     # Transform the data from the file
-    #     student_data = row.split(',')
-    #     student_data = {"FirstName": student_data[0],
-    #                     "LastName": student_data[1],
-    #                     "CourseName": student_data[2].strip()}
-    #     # Load it into our collection (list of lists)
-    #     students.append(student_data)
-
-    # JSON Answer
-    students = json.load(file)
-
-    file.close()
-except Exception as e:
-    print("Error: There was a problem with reading the file.")
-    print("Please check that the file exists and that it is in a json format.")
-    print("-- Technical Error Message -- ")
-    print(e.__doc__)
-    print(e.__str__())
-finally:
-    if file.closed == False:
+# Function to extract the data from the file
+def read_data_from_file(file_name: str, student_data: list):
+    try:
+        file = open(file_name, "r")
+        student_data = json.load(file)
+        for row in student_data:
+            print(f"FirstName: {row['FirstName']}, LastName: {row['LastName']}, Course Name: {row['CourseName']}")
         file.close()
+    except Exception as e:
+        print("Error: There was a problem with reading the file.")
+        print("Please check that the file exists and that it is in a json format.")
+        print("-- Technical Error Message -- ")
+        print(e.__doc__)
+        print(e.__str__())
+    finally:
+        if file.closed == False:
+            file.close()
+    return student_data
+
+
+# Function to save the data to a file
+def write_data_to_file(file_name: str, student_data: list):
+    try:
+        file = open(file_name, "w")
+        json.dump(student_data, file)
+        file.close()
+        print("The following data was saved to file!")
+        for student in student_data:
+            print(f'Student {student["FirstName"]} '
+                  f'{student["LastName"]} is enrolled in {student["CourseName"]}')
+    except Exception as e:
+        if file.closed == False:
+            file.close()
+        print("Error: There was a problem with writing to the file.")
+        print("Please check that the file is not open by another program.")
+        print("-- Technical Error Message -- ")
+        print(e.__doc__)
+        print(e.__str__())
+
 
 # Present and Process the data
-while (True):
+# Function to output the menu
+def output_menu(menu: str):
+    print()  # Adding extra space to make it look nicer.
+    print(menu)
+    print()  # Adding extra space to make it look nicer.
 
-    # Present the menu of choices
-    print(MENU)
-    menu_choice = input("What would you like to do: ")
 
+# Function to input the menu choice
+def input_menu_choice(menu: str):
+    menu_choice = input("Enter your menu choice number: ")
+    print()  # Adding extra space to make it look nicer.
+    return menu_choice
+
+
+# Function to input the student data
+def input_student_data(student_data: list):
     # Input user data
     if menu_choice == "1":  # This will not work if it is an integer!
-
         try:
             student_first_name = input("Enter the student's first name: ")
             if not student_first_name.isalpha():
@@ -94,51 +103,34 @@ while (True):
             print("-- Technical Error Message -- ")
             print(e.__doc__)
             print(e.__str__())
-        continue
 
-    # Present the current data
+
+# Present the current data
+def output_student_courses(student_data: list):
+    print("-" * 50)
+    for student in student_data:
+        print(f'Student {student["FirstName"]} '
+              f'{student["LastName"]} is enrolled in {student["CourseName"]}')
+    print("-" * 50)
+
+# Main Body of Script  ---------------------------------------------------- #
+students= read_data_from_file(file_name=FILE_NAME, student_data=students)
+while True:
+    output_menu(MENU)
+    menu_choice = input_menu_choice(MENU)
+    if menu_choice == "1":
+        input_student_data(student_data=students)
+        continue
     elif menu_choice == "2":
-
-        # Process the data to create and display a custom message
-        print("-" * 50)
-        for student in students:
-            print(f'Student {student["FirstName"]} '
-                  f'{student["LastName"]} is enrolled in {student["CourseName"]}')
-        print("-" * 50)
+        output_student_courses(student_data=students)
         continue
-
-    # Save the data to a file
     elif menu_choice == "3":
-
-        try:
-            file = open(FILE_NAME, "w")
-            # CSV answer
-            # for student in students:
-            #     csv_data = f'{student["FirstName"]},{student["LastName"]},{student["CourseName"]}\n'
-            #     file.write(csv_data)
-
-            # # JSON answer
-            json.dump(students, file)
-
-            file.close()
-            print("The following data was saved to file!")
-            for student in students:
-                print(f'Student {student["FirstName"]} '
-                      f'{student["LastName"]} is enrolled in {student["CourseName"]}')
-        except Exception as e:
-            if file.closed == False:
-                file.close()
-            print("Error: There was a problem with writing to the file.")
-            print("Please check that the file is not open by another program.")
-            print("-- Technical Error Message -- ")
-            print(e.__doc__)
-            print(e.__str__())
+        write_data_to_file(file_name=FILE_NAME,student_data=students)
         continue
-
-    # Stop the loop
     elif menu_choice == "4":
-        break  # out of the loop
+        print("Exiting program.")
+        break
     else:
-        print("Please only choose option 1, 2, or 3")
+        print("Please only choose option 1, 2, 3, or 4")
 
 print("Program Ended")
